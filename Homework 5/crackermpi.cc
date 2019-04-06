578293b99a0b6d40e09dd7243a5693a0beb89c78
Matthew <mc067415@ohio.edu>
2019-04-05 20:39:13 -0400

    project looking close to done

--- Homework 5/crackermpi.cc
+++ Homework 5/crackermpi.cc
@@ -7,8 +7,8 @@
 #include<sstream>
 #include<cstring>
 #include<omp.h>
+#include<cassert>
 #include<mpi.h> 
-#define ROOT 0 // proccess 0 is root  // #TODO make this variable in program.
 
 using namespace std;
 class passcracker{
@@ -28,46 +28,31 @@ class passcracker{
                 ins_salts.open("given/salts");
                 ins_diconary.open("given/words");
                 ins_encrypted.open("given/enc_passwords");
-            }
-            if(argc > 3){
+            }else if(argc > 3){
                 ins_salts.open(argv[1]);
                 ins_diconary.open(argv[2]);
                 ins_encrypted.open(argv[3]);
-            }
-            else{
+            }else{
                 cout << "Please Pass the proper agruments salts,dictonary,encrypted passwords" << endl;
                 assert(0);
             }
 
             string tmp;
-            ins_salts >>tmp;
-            while (!ins_salts.eof()){
+            
+            while (ins_salts >>tmp){
                 salts.push_back(tmp);
-                ins_salts >> tmp;
             }
-            ins_diconary >> tmp;
-            while (!ins_diconary.eof()){
+            while (ins_diconary >> tmp){
                 diconary.push_back(tmp);
-                ins_diconary >> tmp;
             }
-            ins_encrypted >> tmp;
-            while (!ins_encrypted.eof()){
+            while (ins_encrypted >> tmp){
                 encrypted.push_back(tmp);
-                ins_encrypted >> tmp;
-
             }
 
             ins_salts.close();
             ins_diconary.close();
             ins_encrypted.close();
-            for(int t=0;t < diconary.size();t++){
-                if(diconary[t].length() > maxPasswordLength){
-                maxPasswordLength = diconary[t].length() +10;  // the plus 10 to allow for the number in the proccess and other data
-
-                }
-            }
         }
-
 
         void attack(){
             stringstream sstmp;
@@ -96,31 +81,31 @@ class passcracker{
             size_t wordscreated = 0;// use wordscreated to divide the work among the proccesses.
             
             for (int i=0; i<diconary.size(); i++){ 
-                if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                if (wordscreated % world_size == world_rank){     
                     pass(diconary[i]); // word
                 }wordscreated++;    
 
                 for(int a=0; a<single_digit.size();a++){
-                    if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                    if (wordscreated % world_size == world_rank){ 
                         pass(diconary[i]+single_digit[a]); // word + 1digit
                     }wordscreated++;                         
-                    if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                    if (wordscreated % world_size == world_rank){  
                         pass(single_digit[a]+diconary[i]); // 1digit + word     
                     }wordscreated++;                      
                 }
                 for(int b=0; b<double_digit.size(); b++){                 
-                    if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                    if (wordscreated % world_size == world_rank){ 
                         pass(diconary[i]+double_digit[b]); //word + 2digit
                     }wordscreated++;                         
-                    if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                    if (wordscreated % world_size == world_rank){  
                         pass(double_digit[b]+diconary[i]); // 2digit + word
                     }wordscreated++;     
                 }
                 for(int c=0; c<triple_digit.size(); c++){
-                    if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                    if (wordscreated % world_size == world_rank){ 
                         pass(diconary[i]+triple_digit[c]); // word + 3digit
                     }wordscreated++;                         
-                    if ((wordscreated % (world_size-1))+1 == world_rank){ // world_size-1 because the proccessing elemnts are all but root.    
+                    if (wordscreated % world_size == world_rank){ 
                         pass(triple_digit[c]+diconary[i]); // 3digit + word
                     }wordscreated++; 
                 }
@@ -138,28 +123,15 @@ class passcracker{
 
         }
         
-        void end(){
-            if(world_rank!= ROOT ){
-                //cout << "Proccess #" << world_rank << "is about to send the end wordl" << endl;
-                const string keyNotInDiconary = "non-diconary-word";
-                send_message(keyNotInDiconary); // this will hopefully allow proccess 1 to know when it is done.
-            }
-            if(world_rank == ROOT){
-                cout << "The Root proccess has made it to the end function were in the clear." <<endl;
-            }
-        }
 
     private: 
 
-        void pass(string& generated){
+        void pass(string generated){
             for(int i=0; i < salts.size(); i++){
-                if(0 == strcmp(encrypted[i].c_str(), crypt(generated.c_str(),salts[i].c_str()))){ // we found the password matched the encrypted password
-                    if(world_rank!=ROOT){ //if it is the root proccess 
-                        cout <<"rank:" << world_rank  << " found password: " << generated << endl; 
-                        send_message(generated); // send it to the root so it can be displayed
-                    }
+                string cryptedPass = crypt(generated.c_str(),salts[i].c_str());
+                if(encrypted[i] == cryptedPass){ // we found the password matched the encrypted password
+                    cout <<"Rank: #" << world_rank  << " Found password #" << i <<  " it is: "<< generated << endl; 
                 }
-                //cout << "You found Password " << i << " at proccess " << world_rank << " it is " << generated << endl;
             }
         }
 
